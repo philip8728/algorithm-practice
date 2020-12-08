e89ee8300daccfd54bce9cd87335b1691e97b945
Lidai <lidai8728@163.com>
2020-12-08 22:08:03 +0800

    binary search

--- main.go
+++ main.go
@@ -8,12 +8,11 @@ mod path github.com/philip8728/algorithm-practice/pkg
 import (
 	"fmt"
 
-	_mergeSort "github.com/philip8728/algorithm-practice/pkg/sort"
+	_search "github.com/philip8728/algorithm-practice/pkg/search"
 )
 
 func main() {
 	a := []int{10, 5, 6, 7, 8}
-	begin, end := 0, len(a)-1
-	_mergeSort.QuickSort(a, begin, end)
-	fmt.Println(a)
+	target := 6
+	fmt.Println(_search.BinarySearch(a, 0, len(a)-1, target))
 }
--- pkg/search/BinarySearch.go
+++ pkg/search/BinarySearch.go
@@ -1,1 +1,14 @@
 package search
+
+//BinarySearch function
+func BinarySearch(nums []int, begin, end, target int) int {
+	pivot := (begin + end) >> 1
+	if nums[pivot] == target {
+		return pivot
+	} else if target < nums[pivot] {
+		return BinarySearch(nums, 0, pivot-1, target)
+	} else if target >= nums[pivot] {
+		return BinarySearch(nums, pivot+1, end, target)
+	}
+	return -1
+}
